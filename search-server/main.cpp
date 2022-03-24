a92cff298da2e77b6313c8548b321129b5666dd6
Boris <boris.z.077@gmail.com>
2022-03-24 21:50:03 +0300

    test

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,17 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+#include <iostream>
+#include <string>
 
+using namespace  std;
+int main (){
+    int count_three=0;
+    for(int i=1;i<1000;i++){
+        string buff=to_string(i);
+        if (buff.find('3')!= string::npos)
+            count_three++;
+    }
+    cout<<"fish code "<<count_three<<endl;
+    return 0;
+}
+// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
+// Напишите ответ здесь: 271
 // Закомитьте изменения и отправьте их в свой репозиторий.
